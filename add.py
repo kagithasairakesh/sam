3a7508ea7210ebf6f51a5dc07083d6aeae1b1c9d
kireetipydisetty <54438012+kireetipydisetty@users.noreply.github.com>
2019-08-23 16:37:06 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,13 @@
+a=int(input("enter a number "))
+b=int(input("enter a number "))
+c=raw_input("operator ")
+if c=="+":
+    print a+b
+elif c=="-":
+    print a-b
+elif c=="*":
+    print a*b
+elif c=="/":
+    print a/b
+else:
+    print "error"
